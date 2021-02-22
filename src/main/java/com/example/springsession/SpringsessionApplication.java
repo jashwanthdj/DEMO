ec9a3af28a81c0579079fb9ed9421cfb3006850d
Jashwanth Reddy Dodda <jashwanthreddydodda@Jashwanths-MacBook-Pro.local>
2021-02-22 11:27:48 +0530

    ok

--- src/main/java/com/example/springsession/SpringsessionApplication.java
+++ src/main/java/com/example/springsession/SpringsessionApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringsessionApplication {
 
 	public static void main(String[] args) {
+
+     System.out.println("hello");
 		SpringApplication.run(SpringsessionApplication.class, args);
 	}
 
